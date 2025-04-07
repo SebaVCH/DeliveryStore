a678a00d510203e02252775ea429da0078b429e6
PA1IN <d.ramosguerra018@gmail.com>
2025-04-06 20:20:59 -0400

    login y register

--- Frontend/my-app/src/components/login.tsx
+++ Frontend/my-app/src/components/login.tsx
@@ -1,1 +1,8 @@
-import React from "react";
+import React from 'react'
+
+export const login = () => {
+  return (
+    <div>login</div>
+  )
+}
+
--- Frontend/my-app/src/components/register.tsx
+++ Frontend/my-app/src/components/register.tsx
@@ -1,1 +1,7 @@
-import react from 'react';
+import React from 'react'
+
+export const register = () => {
+  return (
+    <div>register</div>
+  )
+}
