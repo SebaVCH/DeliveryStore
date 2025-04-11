45fb954f4b4931863a262bce594cce3abf913f77
SebaVCH <sebastianvchepillo@gmail.com>
2025-04-10 21:58:28 -0400

    fix: Hashing cost
    - Hashing cost update to 10

--- Backend/utils/hashingPass.go
+++ Backend/utils/hashingPass.go
@@ -3,7 +3,7 @@ package utils
 import "golang.org/x/crypto/bcrypt"
 
 func HashPassword(pass string) string {
-	hash, err := bcrypt.GenerateFromPassword([]byte(pass), bcrypt.MinCost)
+	hash, err := bcrypt.GenerateFromPassword([]byte(pass), 10)
 	if err != nil {
 		panic(err)
 	}
