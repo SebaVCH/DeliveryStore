5ea9d93cc4e10d78a17ef59b4d69af5935d138c1
PA1IN <d.ramosguerra018@gmail.com>
2025-04-29 14:24:40 -0400

    actualizacion de fetch a axios en cliente de api

---
+++ Frontend/my-app/src/api/axios.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const api_url = 'http://localhost:8080';
+
+const api = axios.create({
+    baseURL: api_url,
+});
+
+api.interceptors.request.use((config) => {
+    const token = sessionStorage.getItem('token');
+    if(token)
+    {
+        config.headers.Authorization = `${token}`; //sapear el back pa incluir el bearer
+    }
+    return config;
+});
+
+export default api;
+
