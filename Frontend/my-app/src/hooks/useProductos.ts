f532bb9eb079f4da687865d840aa09dc5998d019
BarcobasureroBG <benjamin.garces@alumnos.ucn.cl>
2025-05-28 20:06:43 -0400

    desarrollo productos listo

--- Frontend/my-app/src/hooks/useProductos.ts
+++ Frontend/my-app/src/hooks/useProductos.ts
@@ -1,6 +1,28 @@
 import {useQuery, useMutation, useQueryClient} from '@tanstack/react-query';
 import api from '../api/axios';
 
+interface DataProducto{
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    vegano: boolean;
+    vegetariano: boolean;
+    posee_gluten: boolean;
+    calorias: number;
+    entrega: string;
+    id_vendedor: number;
+}
+
+export function useProductosVendedor(rutUser: string) {     //pa listar solo los productos en venta del usuario.
+    return useQuery({
+        queryKey: ['reserva', rutUser],
+        queryFn: async () => {
+            const respuesta = await api.get('user/productos/',{params:{rut_cliente: rutUser}});
+            return respuesta.data;
+        }
+    });
+}
+
 export function useProductos() {     //pa listar los productos
     return useQuery({
         queryKey: ['productos'],
@@ -15,8 +37,8 @@ export function useProductos() {     //pa listar los productos
 export function useCrearProducto (){ //pa crear un producto
     const clienteQuery = useQueryClient();
     return useMutation({
-        mutationFn: async (nuevoProducto: {nombre: string, precio: number, descripcion: string, entrega: string}) => {
-            const respuesta = await api.post('user/productos',nuevoProducto);
+        mutationFn: async ({nombre, descripcion, precio, vegano, vegetariano, posee_gluten, calorias, entrega, id_vendedor}: DataProducto) => {
+            const respuesta = await api.post('user/productos/',{nombre, descripcion, precio, vegano, vegetariano, posee_gluten, calorias, entrega, id_vendedor});
             return respuesta.data
         },
         onSuccess: () => {
@@ -30,7 +52,7 @@ export function useEliminarProducto(){   //pa eliminar un producto
     const clienteQuery = useQueryClient();
     return useMutation({
         mutationFn: async (id: string) => {
-            await api.delete(`user/productos/${id}`);
+            await api.patch(`user/productos/${id}`);
         },
         onSuccess: () => {
             clienteQuery.invalidateQueries({queryKey:['productos']});
